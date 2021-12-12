14e960e46b7d4fce8f87322f3d9ac2fb1bfce3b8
osiristhomas <73850825+osiristhomas@users.noreply.github.com>
2021-12-12 15:27:47 -0500

    - New files have been added to repository. I forgot to add them before last commit.

---
+++ Core/Inc/wavetables.h
@@ -0,0 +1,19 @@
+/* wavetables.h
+ * Says that there will be global arrays for four lookup tables
+ */
+
+#ifndef _WAVETABLES_H_
+#define _WAVETABLES_H_
+
+#include <stdint.h>
+#include "main.h"
+
+extern const uint16_t sin_lut[NUM_PTS];
+
+extern const uint16_t tri_lut[NUM_PTS];
+
+extern const uint16_t sqr_lut[NUM_PTS];
+
+extern const uint16_t saw_lut[NUM_PTS];
+
+#endif
